8da0ea73b4ef5fae45768fdfd49b4b8ae842bb83
Joran Quinten <joranquinten@hotmail.com>
2015-11-03 11:18:12 +0100

    Cleaned up

--- js/default.js
+++ js/default.js
@@ -8,13 +8,13 @@ var settings = {
 		dilemma__save 	: "../server/dilemma__save.php",
 		dilemma__result : "../server/dilemma__result.php"
 	} ,
-	dilemmaTime : 20 * 1000
+	dilemmaTime : 20 * 1000,
+	timeupResumeNext: true
 };
 
 if (settings.general.debugMode) settings.urls.base_url = window.location.href.split('#')[0];
 
 var dilemmaApp = angular.module('dilemmaApp', ['ngRoute']);
-
 dilemmaApp.config(['$routeProvider',
 	function($routeProvider) {
 	    $routeProvider.
@@ -49,9 +49,12 @@ dilemmaApp.filter('percentage', ['$filter', function ($filter) {
 	  };
 	}]);
 
+/*
+Controllers
+*/
 dilemmaApp.controller('dilemmaStart', function($scope, $location){
-	console.log('dilemmaStart');
 	$scope.dilemmaTime = settings.dilemmaTime;
+
 	$scope.newGame = function(){
 		$location.path('/game');
 	};
@@ -61,49 +64,41 @@ dilemmaApp.controller('dilemmaGame', function($scope, $location, $http, $routePa
 
 		$scope.loadDilemma = function(){
 
-			// (re)set Timer
 			$scope.resetTimer();
 
-			// retrieve dilemma
-			console.log('loadDilemma');
+			// retrieve a random dilemma, or predefined, if hash exists
 			var loadUrl = settings.urls.dilemma__cards;
 			if ($routeParams.guid !== undefined) loadUrl += '?hash='+ $routeParams.guid;
 
 			$http.get(loadUrl)
 				.success(function(response){
 					$scope.dilemmas = response.dilemma.items;
-
-					// Set hash
 					if ($routeParams.guid === undefined){
-						console.log('There is no GUID, set from resource:');
 						if (response.dilemma.guid !== ''){ window.location.hash =  window.location.hash +'/'+ response.dilemma.guid;}
 					}
-					// start the timer on succes!
+
 					$scope.startTimer();
 			});
 		};
 
 		$scope.saveDilemma = function(chosenID){
 			$scope.stopTimer();
-			var qs = '?ts='+ Date.now(); //querystring + timestamp
+			var qs = '?ts='+ Date.now(); // querystring + timestamp
 			if (typeof(chosenID) === 'string') 	qs += '&dilemma='+ chosenID;
 			if ($routeParams.guid !== undefined) qs += '&hash='+ $routeParams.guid;
 
+			// save the chosen option, return a result
 			var loadUrl = settings.urls.dilemma__save;
-
-			console.log(loadUrl + qs);
 
 			$http.get(loadUrl + qs)
 				.success(function(response){
 					if (response.result.success) {
-						// successfully saved, show result
 						$location.path('/result/'+ $routeParams.guid);
 					}
 			});
 		};
 
 		$scope.quitDilemma = function(){
-			console.log('quitDilemma');
 			$scope.resetTimer();
 			$location.path('/quit');
 		};
@@ -121,8 +116,7 @@ dilemmaApp.controller('dilemmaGame', function($scope, $location, $http, $routePa
 						if (iCurrentTime >= iMaxTime){
 							$scope.stopTimer();
 							var timeout = setTimeout(function(){
-								console.log('Load new dilemma');
-								$scope.loadDilemma();
+								if (settings.timeupResumeNext) $scope.loadDilemma();
 							},1000);
 						} else {
 							iCurrentTime += 500;
@@ -154,20 +148,16 @@ dilemmaApp.controller('dilemmaGame', function($scope, $location, $http, $routePa
 
 dilemmaApp.controller('dilemmaResult', function($scope, $http, $location, $routeParams){
 
-	// retrieve dilemma
-	console.log('dilemmaResult');
 	var loadUrl = settings.urls.dilemma__result;
 	if ($routeParams.guid !== undefined) loadUrl += '?hash='+ $routeParams.guid;
 
-	console.log(loadUrl);
-
+	// retrieve result
 	$http.get(loadUrl)
 		.success(function(response){
 			$scope.result = response.result.items;
 	});
 
 	$scope.newDilemma = function(){
-		console.log('newDilemma');
 		$location.path('/game');
 	};
 
@@ -175,14 +165,10 @@ dilemmaApp.controller('dilemmaResult', function($scope, $http, $location, $route
 
 dilemmaApp.controller('dilemmaQuit', function($scope, $location){
 
-	// retrieve dilemma
-	console.log('dilemmaQuit');
 	$scope.homeScreen = function(){
-		console.log('homeScreen');
 		$location.path('/');
 	};
 	$scope.newDilemma = function(){
-		console.log('newDilemma');
 		$location.path('/game');
 	};
 
